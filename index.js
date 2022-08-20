2b38a761b9253707a3b2410ddf3805b003f40e79
luorageoffrey <geoffrey.luora@student.moringaschool.com>
2022-08-20 19:59:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // code your solution here
+function saturdayFun(arg = "roller-skate"){
+    return `This Saturday, I want to ${arg}!`
+}
+
+function mondayWork(arg = "go to the office"){
+
+    return `This Monday, I will ${arg}.`
+
+}
+
+function wrapAdjective(arg){
+
+    if (arg == "*"){
+
+        return function(arg){
+
+            return `You are *${arg}*!`
+    
+        }
+
+    }else if(arg=="||"){
+        return function(arg){
+
+            return `You are ||${arg}||!`
+    
+        }
+    }
+
+    
+
+    
+
+    // if(arg === "*"){
+    //     return "You are *a hard worker*!"
+    // }else if (arg ==="||"){
+
+    // }
+
+}
